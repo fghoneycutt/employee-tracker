5489f14291d47f045c0efe2918b39a2e12767331
fghoneycutt <fghoneycutt@gmail.com>
2022-08-31 01:58:27 -0400

    set up project

---
+++ db/schema.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+    description TEXT
+);
+CREATE TABLE roles(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL 
+    department_id INT FOREIGN KEY 
+);
+CREATE TABLE employee(
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER
+)
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department()
+VALUES
+
+INSERT INTO roles()
+VALUES
+
+INSERT INTO employee()
+VALUES
