3ca716a6128b3e2ee3cd8c662e5108af05baf30c
fghoneycutt <fghoneycutt@gmail.com>
2022-09-09 02:19:56 -0400

    added functions for all choices

--- index.js
+++ index.js
@@ -24,7 +24,49 @@ function chooseAction() {
         ]
     })
         .then(({ action }) => {
-            console.log(action);
+            if (action === "View All Employees") {
+                viewAllEmps();
+            } else if (action === "Add Employee") {
+                addEmployee();
+            } else if (action === "Update Employee Role") {
+                updateEmployee();
+            } else if (action === "View All Roles") {
+                viewAllRoles();
+            } else if (action === "Add Role") {
+                addRole();
+            } else if (action === "View All Departments") {
+                viewDeps();
+            } else if (action === "Add Department") {
+                addDep();
+            }
     })
+}
+function viewAllEmps() {
+    console.log("reached 1")
+    chooseAction();
+}
+function addEmployee() {
+    console.log("reached 2");
+    chooseAction();
+}
+function updateEmployee(){
+    console.log("reached 3");
+    chooseAction();
+}
+function viewAllRoles() {
+    console.log("reached 4");
+    chooseAction();
+}
+function addRole() {
+    console.log("reached 5");
+    chooseAction();
+}
+function viewDeps() {
+    console.log("reached 6");
+    chooseAction();
+}
+function addDep() {
+    console.log("reached 7");
+    chooseAction();
 }
 chooseAction();
