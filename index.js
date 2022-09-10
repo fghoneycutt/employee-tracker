88abbd0a04059671d18112829c59f16ba86c3848
fghoneycutt <fghoneycutt@gmail.com>
2022-09-09 20:58:59 -0400

    connected to mysql db

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+const mysql = require("mysql2");
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '',
+    database: 'employees'
+  },
+  console.log('Connected to the employees database.')
+);
 
 function chooseAction() {
     inquirer.prompt({
