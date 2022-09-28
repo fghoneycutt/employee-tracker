96309c3e19722ca1a8a81fb2cbc0e4a2687479a7
fghoneycutt <fghoneycutt@gmail.com>
2022-09-28 02:22:16 -0400

    view all employees function working

--- db/seeds.sql
+++ db/seeds.sql
@@ -19,10 +19,10 @@ VALUES
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
     ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 1, 1),
-    ('Ashley', 'Rodriguez', 2, NULL),
-    ('Kevin', 'Tupik', 2, 3),
-    ('Kunal', 'Singh', 3, NULL),
-    ('Malia', 'Brown', 3, 5),
-    ('Sarah', 'Lourd', 4, NULL),
-    ('Tom', 'Allen', 4, 7);
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
--- index.js
+++ index.js
@@ -59,17 +59,21 @@ function viewAllEmps() {
         employee.last_name,
         role.title,
         department.name AS department,
-        role.salary, 
-        employee.manager_id AS manager
-      FROM employee 
-      LEFT JOIN role ON role.title 
-    `,
+        role.salary,
+        CONCAT(manager.first_name, " ", manager.last_name) AS manager 
+      FROM employee
+      LEFT JOIN employee manager
+      ON employee.manager_id = manager.id
+      LEFT JOIN role ON employee.role_id = role.id
+      LEFT JOIN department ON role.department_id = department.id;
+      `,
       (err, rows) => {
         console.table(rows);
         chooseAction();
       }
     );
 }
+
 function addEmployee() {
     
     inquirer
